8b505f9d7ae48cc8ab201e3a96a403ba923242b8
Christian Busch <cb@oncrea.de>
2026-02-13 01:41:09 +0100

    'asgi'

--- module_bundler.py
+++ module_bundler.py
@@ -11,14 +11,15 @@ class module_bundler(GDO_Module):
 
     def __init__(self):
         super().__init__()
-        self._priority = 200
+        self._priority = 2088
 
     def gdo_module_config(self) -> list[GDT]:
         return [
-
         ]
 
-    def gdo_load_scripts(self, page: 'GDT_Page'):
+    def gdo_install(self):
         Files.create_dir(Application.file_path(f'assets/{GDO_Module.CORE_REV}/'))
+
+    def gdo_load_scripts(self, page: 'GDT_Page'):
         JSMinifier(GDT_Page._js).execute()
         CSSMinifier(GDT_Page._css).execute()
